d4e295bd9a1a4fc456e58b469952cb59832d1a37
ClushXD <adarshjaiswalofficial06@gmail.com>
2026-05-08 11:22:22 +0530

    Update game.py

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import random
 
 # NEW: Tell Flask to use the "interface" folder for HTML, CSS, and JS
 game = Flask(__name__, template_folder="interface", static_folder="interface")
-
+count=0
 target_number = random.randint(1, 100)
 
 @game.route("/")
@@ -18,14 +18,17 @@ def guess():
     diff = abs(target_number - user_guess)
 
     if user_guess == target_number:
-        message = f"🎉 Correct! The number was {target_number}. Starting a new game!"
+        message = f"🎉 Correct! The number was {target_number} and u guessed it in {count} tries. Starting a new game!"
         target_number = random.randint(1, 100)
     elif diff <= 5:
         message = "🔥 Very close! Think " + ("higher ↑" if user_guess < target_number else "lower ↓")
+        count+=1
     elif diff <= 15:
         message = "🙂 Close! Try " + ("higher ↑" if user_guess < target_number else "lower ↓")
+        count+=1
     else:
         message = "⬆️ Too low!" if user_guess < target_number else "⬇️ Too high!"
+        count+=1
 
     return jsonify({"message": message})
 
